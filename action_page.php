0208bd0e9e368aee008a60373295babf8b5d927a
evoo15 <superyassine15@gmail.com>
2018-03-28 23:06:00 +0100

    database and login verification

--- action_page.php
+++ action_page.php
@@ -1,9 +1,40 @@
-<html>
-welcome
-<head>
 
-</head>
-<body>
-<?php echo $_POST["username"] ?>
-</body>
-</html>
+<?php
+
+require 'db.php';
+session_start();
+?>
+
+<?php
+/* User login process, checks if user exists and password is correct */
+
+// Escape email to protect against SQL injections
+$id = $conn->escape_string($_POST['username']);
+$result = $conn->query("SELECT * FROM users WHERE id='$id'");
+
+if ( $result->num_rows == 0 ){ // User doesn't exist
+    echo "User with that id doesn't exist!";
+
+}
+else { // User exists
+    $user = $result->fetch_assoc();
+
+    if ($_POST['password']==$user['password'])  {
+
+        $_SESSION['id'] = $user['id'];
+        $_SESSION['Nom'] = $user['Nom'];
+        $_SESSION['Prenom'] = $user['Prenom'];
+
+
+        // This is how we'll know the user is logged in
+        $_SESSION['logged_in'] = true;
+
+        echo "welcome " .$user['Nom'] .' ' . $user['Prenom'] ;
+
+    }
+    else {
+        echo  "You have entered wrong password, try again!";
+        echo "password is " .$user['password'] .$_POST['password'] ;
+         }
+}
+
---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+
+/* Database connection settings */
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'accounts';
+
+// Create connection
+$conn = new mysqli($host,$user,$pass,$db) ;
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected to database\n";
