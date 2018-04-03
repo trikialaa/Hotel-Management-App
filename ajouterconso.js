3776248d435627608aab52715b9fb88e84db684c
olfa-koubaa <35567768+olfa-koubaa@users.noreply.github.com>
2018-04-03 22:23:12 +0100

    Add files via upload

---
+++ ajouterconso.js
@@ -0,0 +1,12 @@
+
+function ouvrirconso()
+{
+    var dialogconso =document.getElementById("consodialogue");
+    dialogconso.showModal();
+}
+
+function fermerconso()
+{
+    var x =document.getElementById("consodialogue");
+   x.close();
+}
