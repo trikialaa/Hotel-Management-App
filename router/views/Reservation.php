52cfc1f17c40e003c8900dd940d9a695052dd0d6
Olfa koubaa <koubaaolfa1@gmail.com>
2018-04-19 20:21:26 +0100

    j'ai séparé les deux fichiers reservation

---
+++ router/views/Reservation.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: olfa
+ * Date: 19/04/2018
+ * Time: 20:19
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Reservation</title>
+
+</head>
+<body>
+<form method="POST" action="../public/php/controlClass/Reservation.php" >
+    <div class="form-group">
+        <h2 class="heading">Effectuer réservation</h2>
+
+        <div class="controls">
+            <input type="text" id="cin" class="floatLabel" name="cin" required>
+            <label for="cin">CIN</label>
+        </div>
+
+        <div class="controls">
+            <input type="tel" id="phone" class="floatLabel" name="phone">
+            <label for="phone">Tel</label>
+        </div>
+
+    </div>
+
+    <div class="grid">
+
+        <div class="col-1-4 col-1-4-sm">
+            <div class="controls">
+                <input type="date" id="arrive" class="floatLabel" name="arrive" value="<?php echo date('Y-m-d'); ?>">
+                <label for="arrive" class="label-date"><i class="fa fa-calendar"></i>&nbsp;&nbsp;Date d'arrivé</label>
+            </div>
+        </div>
+        <div class="col-1-4 col-1-4-sm">
+            <div class="controls">
+                <input type="date" id="depart" class="floatLabel" name="depart" value="<?php echo date('Y-m-d'); ?>" />
+                <label for="depart" class="label-date"><i class="fa fa-calendar"></i>&nbsp;&nbsp;Date de départ</label>
+            </div>
+        </div>
+    </div>
+    <div class="controls">
+        <select class="floatLabel" name="liste" id="liste">
+            <option value="chambre single">single</option>
+            <option value="chambre double">double</option>
+            <option value="chambre triple">triple</option>
+        </select>
+        <label for="liste">Type chambre</label>
+    </div>
+    <div class="grid">
+        <input type="submit" name="submit" value="Résever" id="log">
+    </div>
+
+</form>
+
+
+</body>
+</html>
