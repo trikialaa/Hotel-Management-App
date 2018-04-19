6757cc91728b2b8d7a760a4af2ab2d74c445aa61
Olfa koubaa <koubaaolfa1@gmail.com>
2018-04-19 23:53:29 +0100

    forms style

--- router/views/Reservation.php
+++ router/views/Reservation.php
@@ -11,6 +11,12 @@
 <head>
     <meta charset="UTF-8">
     <title>Reservation</title>
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+    <link href='https://fonts.googleapis.com/css?family=Lato:300,400' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+    <link rel="stylesheet" href="css/form_check_in.css">
 
 </head>
 <body>
@@ -19,13 +25,13 @@
         <h2 class="heading">Effectuer réservation</h2>
 
         <div class="controls">
-            <input type="text" id="cin" class="floatLabel" name="cin" required>
-            <label for="cin">CIN</label>
+            <b> <i class="material-icons">person</i> &nbsp;&nbsp; Cin Client: </b>
+            <input type="text" id="cin" class="floatLabel" name="cin" placeholder="CIN" required>
         </div>
 
         <div class="controls">
-            <input type="tel" id="phone" class="floatLabel" name="phone">
-            <label for="phone">Tel</label>
+            <b> <i class="material-icons">phone</i> &nbsp;&nbsp; Num Tel client</b>
+            <input type="tel" id="phone" class="floatLabel" name="phone" placeholder="Tel">
         </div>
 
     </div>
@@ -34,27 +40,29 @@
 
         <div class="col-1-4 col-1-4-sm">
             <div class="controls">
+                <b> <i class="fa fa-calendar"></i>&nbsp;&nbsp;Arrivé </b>
                 <input type="date" id="arrive" class="floatLabel" name="arrive" value="<?php echo date('Y-m-d'); ?>">
-                <label for="arrive" class="label-date"><i class="fa fa-calendar"></i>&nbsp;&nbsp;Date d'arrivé</label>
+
             </div>
         </div>
         <div class="col-1-4 col-1-4-sm">
             <div class="controls">
+                <b> <i class="fa fa-calendar"></i>&nbsp;&nbsp;Départ </b>
                 <input type="date" id="depart" class="floatLabel" name="depart" value="<?php echo date('Y-m-d'); ?>" />
-                <label for="depart" class="label-date"><i class="fa fa-calendar"></i>&nbsp;&nbsp;Date de départ</label>
+
             </div>
         </div>
     </div>
     <div class="controls">
+        <b > <i class="fa fa-home"></i>&nbsp;&nbsp;Type chambre </b>
         <select class="floatLabel" name="liste" id="liste">
             <option value="chambre single">single</option>
             <option value="chambre double">double</option>
             <option value="chambre triple">triple</option>
         </select>
-        <label for="liste">Type chambre</label>
     </div>
     <div class="grid">
-        <input type="submit" name="submit" value="Résever" id="log">
+        <button type="submit" value="Submit" class="col-1-4">Valider</button>
     </div>
 
 </form>
