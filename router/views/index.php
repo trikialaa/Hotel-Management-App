5dbc76412f4e46bb5871e72c504e5b5bb2a825ff
Ali Hamouda <36375682+alichamouda@users.noreply.github.com>
2018-04-20 10:00:00 +0100

    j'ai séparé les deux fichiers reservation

---
+++ router/views/index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hamouda
+ * Date: 20/04/2018
+ * Time: 08:13
+ */
+
+    echo "<h1> URIs LIST</h1>";
+    echo '<table>';
+    echo "<tr><td width='150px'>".'/'."<td>".'views/index.php'."</td></tr>";
+    echo "<tr><td>".'/home'."<td>".'views/home.php'."</td></tr>";
+    echo "<tr><td>".'/login'."<td>".'views/login.php'."</td></tr>";
+    echo "<tr><td>".'/reservation'."<td>".'views/Reservation.php'."</td></tr>";
+    echo "<tr><td>".'/singlerooms'."<td>".'views/Singlerooms.php'."</td></tr>";
+    echo "<tr><td>".'/elfac'."<td>".'public/php/controlClass/elementfacturetab.php'."</td></tr>";
+    echo "<tr><td>".'/checkin'."<td>".'views/CheckInForm.php'."</td></tr>";
+    echo "<tr><td>".'/checkcin'."<td>".'views/CheckCIN.php'."</td></tr>";
+    echo "<tr><td>".'/consomation'."<td>".'views/consommation.php'."</td></tr>";
+    echo '</table>';
