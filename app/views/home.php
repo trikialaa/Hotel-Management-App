d39a7590e3e49446f3ab437405745fd846f0c79c
zeineb97 <31375483+zeineb97@users.noreply.github.com>
2018-04-28 23:44:45 +0200

    some design sttaf

--- app/views/home.php
+++ app/views/home.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <title>Home Page </title>
 
     <link rel="stylesheet" href="app/css/homepage.css" type="text/css">
-    <title></title>
+    <link rel="stylesheet" href="app/css/font-awesome.css">
+    <link href='app/css/css.css' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="app/css/icon.css">
+
+    <link rel="stylesheet" href="app/css/form_check_in.css">
+
 </head>
 <body>
 <div>
@@ -11,10 +18,10 @@
         include("navbar.php");
     ?>
 
-
+    <h2>Choisir chambre</h2>
 
     <div>
-        <h2>choisir chambre</h2>
+
         <img width="300px" src="img/room1.png"  class="chambre"/>
         <img width="300px" src="img/room2.png"  class="chambre"/>
         <br>
@@ -22,9 +29,10 @@
         <img width="300px" src="img/room4.png" class="chambre"/>
 
     </div>
+    <br><br><br><br>
 
     <div>
-        <button id="facture">génerer facture</button>
+        <button id="facture" class="col-1-4">Génerer facture</button>
     </div>
 
     <br><br><br><br>
@@ -36,6 +44,11 @@
     </div>
 
 </div>
-
+<script src='app/js/jquery-1.7.2.min.js'></script>
+<script src="app/js/form_check_in.js"></script>
+<script src='app/js/jquery.min.js'></script>
+<script src='app/js/jquery-ui-autocomplete.js'></script>
+<script src='app/js/jquery-ui-autocomplete.js'></script>
+<script src='app/js/jquery.select-to-autocomplete.min.js'></script>
 </body>
 </html>
