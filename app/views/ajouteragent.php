b589bc28d7e2b5881c11c3a738b95be515f42720
Olfa koubaa <koubaaolfa1@gmail.com>
2018-04-28 21:25:59 +0100

    ajouter agent page

---
+++ app/views/ajouteragent.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ajouter agent</title>
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+    <link href='https://fonts.googleapis.com/css?family=Lato:300,400' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+    <link rel="stylesheet" href="app/css/form_check_in.css">
+</head>
+<body>
+
+
+<form>
+    <h2 class="heading">Ajouter agent</h2>
+    <input type="text" placeholder="login agent" class="floatLabel" required>
+    <input type="password" placeholder="mot de passe agent" class="floatLabel" required>
+    <div>
+        <button type="submit" class="col-1-4">confirmer</button>
+        <button type="reset" class="col-1-4">annuler</button>
+    </div>
+</form>
+
+
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js' type="javascript"></script>
+<script src='http://raw.githubusercontent.com/andiio/selectToAutocomplete/master/jquery-ui-autocomplete.js'
+        type="javascript"></script>
+<script src='http://raw.githubusercontent.com/andiio/selectToAutocomplete/master/jquery.select-to-autocomplete.js'
+        type="javascript"></script>
+<script src='http://raw.githubusercontent.com/andiio/selectToAutocomplete/master/jquery.select-to-autocomplete.min.js'
+        type="javascript"></script>
+
+
+</body>
+</html>
